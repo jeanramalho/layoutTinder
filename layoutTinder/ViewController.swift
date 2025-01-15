902c8626a55503c13f5202c12be5ed83741c6008
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-01-15 13:45:31 -0300

    [add] - Inicia projeto

---
+++ layoutTinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  layoutTinder
+//
+//  Created by Jean Ramalho on 15/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
