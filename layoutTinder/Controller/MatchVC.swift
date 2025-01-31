de0f01c5a08a0596cbc8b50c41afeee693297082
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-01-31 11:23:37 -0300

    [add] - Implementa elementos dentro do modal

--- layoutTinder/Controller/MatchVC.swift
+++ layoutTinder/Controller/MatchVC.swift
@@ -16,6 +16,74 @@ class MatchVC: UIViewController {
         return image
     }()
     
+    lazy var likeImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "icone-like")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    lazy var mensagemLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 18)
+        label.text = "Ana curtiu você também!"
+        label.textAlignment = .center
+        label.layer.shadowColor = UIColor.black.cgColor
+        label.layer.shadowRadius = 2.0
+        label.layer.shadowOpacity = 0.8
+        label.layer.shadowOffset = CGSize(width: 1, height: 1)
+        label.layer.masksToBounds = false
+        return label
+    }()
+    
+    lazy var infoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        return stackView
+    }()
+    
+    lazy var mensagemTxt: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Diga algo legal..."
+        textField.backgroundColor = .white
+        textField.layer.cornerRadius = 8
+        textField.textColor = .darkText
+        textField.returnKeyType = .go
+        
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 0))
+        textField.leftViewMode = .always
+        
+        textField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 90, height: 0))
+        textField.rightViewMode = .always
+        return textField
+    }()
+    
+    lazy var mensagemEnviarButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Enviar", for: .normal)
+        button.setTitleColor(UIColor(red: 62/255, green: 163/255, blue: 255/255, alpha: 1), for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        return button
+    }()
+    
+    lazy var voltarButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Voltar para o Tinder", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,6 +97,14 @@ class MatchVC: UIViewController {
     
     private func setHierarchy(){
         view.addSubview(fotoImageView)
+        view.addSubview(infoStackView)
+        
+        infoStackView.addArrangedSubview(likeImageView)
+        infoStackView.addArrangedSubview(mensagemLabel)
+        infoStackView.addArrangedSubview(mensagemTxt)
+        infoStackView.addArrangedSubview(voltarButton)
+        
+        mensagemTxt.addSubview(mensagemEnviarButton)
     }
     
     private func setConstraints(){
@@ -37,6 +113,20 @@ class MatchVC: UIViewController {
             fotoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             fotoImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             fotoImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            infoStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            infoStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            infoStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -46),
+            
+            likeImageView.heightAnchor.constraint(equalToConstant: 44),
+            
+            mensagemTxt.heightAnchor.constraint(equalToConstant: 44),
+            
+            mensagemEnviarButton.topAnchor.constraint(equalTo: mensagemTxt.topAnchor),
+            mensagemEnviarButton.bottomAnchor.constraint(equalTo: mensagemTxt.bottomAnchor),
+            mensagemEnviarButton.trailingAnchor.constraint(equalTo: mensagemTxt.trailingAnchor, constant: -16),
+            
+            
         ])
     }
 }
