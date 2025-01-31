4b7b9d68cd90503440e0100cb26439b9bc8c55fe
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-01-31 10:17:41 -0300

    [add] - Inicia a implementação de do modal de match

---
+++ layoutTinder/Controller/MatchVC.swift
@@ -0,0 +1,14 @@
+//
+//  MatchVC.swift
+//  layoutTinder
+//
+//  Created by Jean Ramalho on 31/01/25.
+//
+import Foundation
+import UIKit
+
+class MatchVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
