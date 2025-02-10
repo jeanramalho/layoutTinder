0158e53a6eb205394bd0ff1e37d24fdae57f2d39
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-02-10 08:10:52 -0300

    [add] - Cria viewController da tela de detalhe

---
+++ layoutTinder/Controller/DetalheVC.swift
@@ -0,0 +1,23 @@
+//
+//  DetalheVC.swift
+//  layoutTinder
+//
+//  Created by Jean Ramalho on 10/02/25.
+//
+import Foundation
+import UIKit
+
+class DetalheVC: UICollectionViewController {
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
