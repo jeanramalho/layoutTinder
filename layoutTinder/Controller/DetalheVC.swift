79c944b0166198b53662b467c8720550a4f3f867
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-02-10 09:24:37 -0300

    [feat] - Implementa collectionview no modal de detalhes, separa na arquitetura a header do modal, implementa foto no no header do modal

--- layoutTinder/Controller/DetalheVC.swift
+++ layoutTinder/Controller/DetalheVC.swift
@@ -7,7 +7,7 @@
 import Foundation
 import UIKit
 
-class DetalheVC: UICollectionViewController {
+class DetalheVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     let cellId = "cellId"
     let headerId = "headerId"
@@ -25,19 +25,35 @@ class DetalheVC: UICollectionViewController {
         setup()
     }
     
-    private func setup(){
-        collectionView.register(UICollectionView.self, forCellWithReuseIdentifier: cellId)
-        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
+    private func setup() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(DetalheHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
+        collectionView.contentInsetAdjustmentBehavior = .never
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 3
     }
     
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionHeader else { return UICollectionReusableView() }
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath) as! DetalheHeaderView
+
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: view.bounds.width, height: view.bounds.height * 0.7)
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .blue
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.bounds.width, height: 100)
+    }
     
 }
---
+++ layoutTinder/View/DetalheHeaderView.swift
@@ -0,0 +1,49 @@
+//
+//  DetalheHeaderView.swift
+//  layoutTinder
+//
+//  Created by Jean Ramalho on 10/02/25.
+//
+import Foundation
+import UIKit
+
+class DetalheHeaderView: UICollectionReusableView {
+    
+    lazy var fotoImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "pessoa-1")
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        self.addSubview(fotoImageView)
+    }
+    
+    private func setConstraints(){
+        NSLayoutConstraint.activate([
+            fotoImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            fotoImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            fotoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            fotoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+        ])
+    }
+}
