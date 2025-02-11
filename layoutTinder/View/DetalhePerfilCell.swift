9618cbd8c246a29b4b43d40102ee38b5de9fd4cd
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-02-11 09:59:02 -0300

    [add] - Inicia a construção da detalhePerfilCell

---
+++ layoutTinder/View/DetalhePerfilCell.swift
@@ -0,0 +1,18 @@
+//
+//  DetalhePerfilCell.swift
+//  layoutTinder
+//
+//  Created by Jean Ramalho on 11/02/25.
+//
+import Foundation
+import UIKit
+
+class DetalhePerfilCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
